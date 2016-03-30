42361c80c7f641a29f082ccc4dd04dd1ef45c05b
Kemal Colovic <ddeveloperr@gmail.com>
2016-03-30 21:21:41 +0000

    :tea: Add contacts route as resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+   resources :contacts
    get '/about' => 'pages#about'
    root 'pages#home'
    
