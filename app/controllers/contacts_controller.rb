5b67433c4fd2b1cd0b234ca45d718556201fa5b1
Kemal Colovic <ddeveloperr@gmail.com>
2016-04-01 21:32:57 +0000

    :tea: Add new form, flash messages, validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,17 +6,27 @@ class ContactsController < ApplicationController
     def create
         @contact = Contact.new(contact_params)
         
+        ## Received all details from form and save it
         if @contact.save
+            # evaluate true and get us success message
             flash[:success] = "Message sent"
+            # then redirect to 
             redirect_to new_contact_path
         else
+            # if there some errors, it's not save, it's evaluate false provide to us error message
             flash[:danger] = "Error occurd, message has not been sent!"
+            # redirect us to
             redirect_to new_contact_path
         end
     end
     
     private 
+      # The params come from the user's browser when they request the page.
       def contact_params
+          # Called as white list, it improved rails security
+          # More details for params http://stackoverflow.com/questions/6885990/rails-params-explained
+          
+          # Tou can filter parameter which 
           params.require(:contact).permit(:name, :email, :commands)
       end
 end
