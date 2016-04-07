8d1ac4aed6dd6ac846e3941c225bc7046e6580db
Kemal Colovic <ddeveloperr@gmail.com>
2016-04-07 12:09:35 +0000

    :key: Update navbar with user links,  Add if logic to follow WMI user

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,13 +1,14 @@
 class ContactMailer < ActionMailer::Base
     default to: 'ddeveloperr@gmail.com'
     
+    # This method pull all info from form
     def contact_email(name, email, body)
       @name = name
       @email = email
       @body = body
     
     
-    mail(form: email, subject: 'Contact Form Message')
+    mail(from: email, subject: 'Contact Form Message')
     end
     
 end
