eb4a7a0094375659a0627247a1f7b7ba29ee56df
Kemal Colovic <ddeveloperr@gmail.com>
2016-04-08 21:43:06 +0000

    :credit_card: Add association for plans and users

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,3 +1,3 @@
 class Plan < ActiveRecord::Base
-    
+    has_many :users # users is plural
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407211325) do
+ActiveRecord::Schema.define(version: 20160408213748) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160407211325) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
